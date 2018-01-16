2818332c3df0f88b24b10af7054611c01d5ce0df
TetraDev01 <tetradev01@tetratutorials.com>
2018-01-16 15:24:39 +0000

    updated ant script.

--- dslscripts/antjobs.groovy
+++ dslscripts/antjobs.groovy
@@ -6,6 +6,9 @@ job('antbuild') {
     steps {
         ant {
             targets(['clean', 'init', 'compile', 'war'])
+            prop('version', '0.1-dev')
+            prop('source', '1.7')
+            prop('target', '1.7')
             buildFile('build.xml')
             javaOpt('-Xmx1g')
             javaOpts(['-Dprop2=value2', '-Dprop3=value3'])
