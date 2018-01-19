5bb1da7c28f4078f928d2795dfb8552f4626723f
root <root@vagrant-ubuntu-precise-64.(none)>
2018-01-19 20:45:13 +0000

    script updated

--- dslscripts/antjobs.groovy
+++ dslscripts/antjobs.groovy
@@ -27,6 +27,17 @@ job('deploy') {
      * configuring ssh plugin to run docker commands
      */
     steps{
-             shell 'sshpass -p "123456" scp /var/lib/jenkins/workspace/antbuild/build/helloworld-0.1-dev.war release@192.168.44.8:/var/lib/tomcat7/webapps'
+             shell 'sshpass -p "123456" scp /var/lib/jenkins/workspace/antbuild/build/helloworld-0.1-dev.war release@192.168.44.8:/home/release'
       }
+
+      configure { project ->
+        project / buildWrappers / 'org.jvnet.hudson.plugins.SSHBuildWrapper' {
+            siteName 'release@imaginarium.lexandro.com:22'
+            postScript """
+            echo 'coping...'
+            sudo sshpass -p '123456' cp /home/release/helloworld-0.1-dev.war /var/lib/tomcat7/webapps
+            echo 'copied'
+            """
+        }
+    }
 }
