3a9f4cd00c72d5daa7788090a0713baea737d5ed
tetradev01 <tetradev01@tetratutorials.com>
2018-01-20 09:13:48 +0000

    cp command changed.

--- dslscripts/antjobs.groovy
+++ dslscripts/antjobs.groovy
@@ -5,7 +5,6 @@ job('antbuild') {
 
     steps {
         ant {
-            targets(['clean', 'init', 'compile', 'war'])
             prop('version', '0.1-dev')
             prop('source', '1.7')
             prop('target', '1.7')
