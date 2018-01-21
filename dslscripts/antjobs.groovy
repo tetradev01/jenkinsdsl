d2a974299690e55bed7f48c52cc2012543837219
tetradev01 <tetradev01@tetratutorials.com>
2018-01-21 18:15:14 +0530

    sudo added

--- dslscripts/antjobs.groovy
+++ dslscripts/antjobs.groovy
@@ -22,6 +22,6 @@ job('deploy') {
      * configuring ssh plugin to run docker commands
      */
     steps{
-             shell 'sshpass -p \'123456\' scp /var/lib/jenkins/workspace/antbuild/build/helloworld-dev.war release@10.12.108.11:/opt/tomcat/webapps/'
+             shell 'sudo sshpass -p \'123456\' scp /var/lib/jenkins/workspace/antbuild/build/helloworld-dev.war release@10.12.108.11:/opt/tomcat/webapps/'
       }
 }
