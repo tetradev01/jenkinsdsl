d5f98f98309382885fe3d58ae1f83b38707468d4
tetradev01 <tetradev01@tetratutorials.com>
2018-02-04 15:17:45 +0000

    nodejs groovy committed.

---
+++ dslscripts/nodejsdsl.groovy
@@ -0,0 +1,66 @@
+job('nodejs-dsl-checkout') {
+    
+    scm {
+        github('azat-co/nodejs-hello-world', 'master')
+    }
+  
+   publishers {
+        downstream 'nodejs-dsl-install', 'SUCCESS'
+    }
+}
+
+job('nodejs-dsl-install') {
+	customWorkspace('/var/lib/jenkins/workspace/nodejs-dsl-checkout')
+
+	steps{
+		shell 'npm install'
+	}
+
+	publishers{
+		downstream 'nodejs-dsl-test', 'SUCCESS'
+	}
+}
+
+job('nodejs-dsl-test'){
+	customWorkspace('/var/lib/jenkins/workspace/nodejs-dsl-checkout')
+
+	steps{
+		shell 'npm start &'
+		shell 'npm test'
+		shell 'npm stop'
+	}
+
+	publishers{
+		downstream 'nodejs-dsl-archive', 'SUCCESS'
+	}
+}
+
+
+job('nodejs-dsl-archive'){
+	configure { project ->
+        project / buildWrappers / 'org.jvnet.hudson.plugins.SSHBuildWrapper' {
+            siteName 'release@10.12.108.11:22'
+            postScript """        
+            	tar -zcvf /var/archive/$(date '+%Y%m%d-%h%m%s').tar.gz /var/myapp
+            	rm -rf /var/myapp/*
+				"""
+        }
+	}
+
+	publishers{
+		downstream 'nodejs-dsl-deploy', 'SUCCESS'
+	}
+}
+
+job('nodejs-dsl-deploy'){
+
+	configure { project ->
+        project / buildWrappers / 'org.jvnet.hudson.plugins.SSHBuildWrapper' {
+            siteName 'release@10.12.108.11:22'
+            postScript """
+            	cd /var/myapp
+            	git pull origin master
+				"""
+        }
+    }
+}
